693e6f4edd260d3d822095cf5a4189a63d58cc1a
nonress <b.nxxxi551@gmail.com>
2020-02-05 14:07:19 +0900

    add: Commentモデルにhas_manyとvalidatesを追加

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :article
+  has_many :favorite, :as => :favoriteable, dependent: :destroy
+  validates :user_id, presence: true
+  validates :body, presence: true, length: { maximum: 10 }
 end
