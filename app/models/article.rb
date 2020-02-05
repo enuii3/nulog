35f7007356ccc1a25a56623e59051195ad53f8be
nonress <b.nxxxi551@gmail.com>
2020-02-05 14:28:57 +0900

    fix: rubocopによる修正

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,8 @@
 class Article < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
-  has_many :favorite, :as => :favoriteable, dependent: :destroy
+  has_many :favorite, as: :favoriteable, dependent: :destroy
   validates :user_id, presence: true
   validates :title, presence: true, length: { maximum: 8 }
-  validates :body, presence: true, length: { maximum: 10}
+  validates :body, presence: true, length: { maximum: 10 }
 end
