fb69b743267f8d7bc31af9d3cbec66753e924286
nonress <b.nxxxi551@gmail.com>
2020-02-05 14:03:49 +0900

    add: Userモデルにhas_manyとvalidatesを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :articles, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :favorite, :as => :favoriteable, dependent: :destroy
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
 end
