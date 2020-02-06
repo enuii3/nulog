b748b13125e89c186a99b3508ef6ee0a053225d3
nonress <b.nxxxi551@gmail.com>
2020-02-06 18:20:13 +0900

    fix: UserモデルのスペックとFactoryBotの作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
-    
+    name { 'foobar' }
+    email { 'sample@hogehoge.com' }
+    encrypted_password { 'hogehoge' }
+  end
+
+  factory :other, class: User do
+    name { 'mickel hartle' }
+    email { 'foobar@hogehoge.com' }
+    encrypted_password { 'hogehoge' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user){FactoryBot.create(:user)}
+
+  #名前とメールアドレスとパスワードがあれば有効
+  it 'is valid with name, email, and encrypted_password' do
+    user.valid?
+    expect(user).to be_valid
+  end
+
+  #名前が無ければ無効
+  it 'is invalid without a name' do
+    user.update(name: nil)
+    user.valid?
+    expect(user.errors[:name]).to include("can't be blank")
+  end
+
+  #メールアドレスが無ければ無効
+  it 'is invalid without an email' do
+    user.update(email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+
+  #パスワードが無ければ無効
+  it 'is invalid without an encrypted_password' do
+    user.update(encrypted_password: nil)
+    user.valid?
+    expect(user.errors[:encrypted_password]).to include("can't be blank")
+  end
+
+  #メールアドレスが重複していれば無効
+  it 'is invalid with duplicate email' do
+    user
+    other = User.create(
+      name: 'nakai',
+      email: 'sample@hogehoge.com',
+      encrypted_password: 'hogehoge',
+    )
+    other.valid?
+    expect(other.errors.messages).to include("has already been taken")
+  end
 end
