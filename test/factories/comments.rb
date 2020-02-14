cd84cae8fe5f65ad27511cf1a7e1c3c7bd79241e
nonress <b.nxxxi551@gmail.com>
2020-02-14 17:59:34 +0900

    fix: comment factoryのコメントの修正

--- test/factories/comments.rb
+++ test/factories/comments.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :comment do
     body { Faker::Lorem.paragraph_by_chars }
     association :article
-    # comment facotory is at a lateあ
+    # comment facotory is at a late
   end
 end
