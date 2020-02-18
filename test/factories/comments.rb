446d043dda7d299961eaafe08872ca8a3ab29c56
nonress <b.nxxxi551@gmail.com>
2020-02-18 12:54:53 +0900

    fix: アノテーションコメントの修正

--- test/factories/comments.rb
+++ test/factories/comments.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :comment do
     body { Faker::Lorem.paragraph_by_chars }
     association :article
-    # TODO: "現状作成できないのでモデル側を含め、後日対応"
+    # TODO: 現状作成できないのでモデル側を含め、後日対応
   end
 end
