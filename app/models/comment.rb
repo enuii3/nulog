2fa2311feed03e44e7673f808ad38fee6a6c7b72
nonress <46844364+nonress@users.noreply.github.com>
2020-02-18 14:50:17 +0900

    fix: 匿名ユーザーがコメント出来る様にするための修正 (#22)
    * fix: Commentモデルのuser_idカラムにnilの許可を追加
    
    * add: commentsテーブルのuser_idカラムにnullの許可
    
    * add: マイグレーションを実行
    
    * delete: comment factoryが作成出来る様になったのでコメントアウトを削除

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   belongs_to :article
   has_many :favorite, as: :favoriteable, dependent: :destroy
   validates :body, presence: true, length: { maximum: 1000 }
