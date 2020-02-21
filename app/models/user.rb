e5d9b6b0113c7513a06061d2fb0d4b2c615ce6f2
nonress <b.nxxxi551@gmail.com>
2020-02-22 00:37:10 +0900

    chore: deviseによるModuleの選定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,5 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable
   has_many :articles, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :favorite, as: :favoriteable, dependent: :destroy
