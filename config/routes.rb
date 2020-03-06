664f67b866f9e18989c184d300d9aa17f4a28193
nonress <b.nxxxi551@gmail.com>
2020-03-06 19:10:28 +0900

    chore: 正しいapiのarticleのルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :articles
+  namespace :api, { format: 'json'} do
+    namespace :v1 do
+      resources :articles
+    end
+  end
 end
