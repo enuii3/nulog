d8f57c9232512366eb345878eda41a26b6b3dc7c
nonress <b.nxxxi551@gmail.com>
2020-03-07 18:33:36 +0900

    chore: articlesをフロント側に反映させるためのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  namespace :api, { format: 'json'} do
-    namespace :v1 do
-      resources :articles
-    end
-  end
+  resources :articles
+
+   namespace :api, { format: 'json'} do
+     namespace :v1 do
+       resources :articles
+     end
+   end
 end
