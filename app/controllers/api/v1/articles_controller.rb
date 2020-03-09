a1ed024bea21134529747e6219333ef1e6a12598
nonress <b.nxxxi551@gmail.com>
2020-03-09 16:09:08 +0900

    Revert "chore: api側のコントローラーにmessageプロパティを付与"
    This reverts commit f98641588de1f34faafc1be2ce34c2d3e69baf49.

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::ArticlesController < ApplicationController
   def index
     @articles = Article.all
-    render json: { message: @articles }
+
+    render json: @articles
   end
 end
