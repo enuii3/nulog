f4dfc5d45aee49221fceb9e804250b75ccc27df5
nonress <b.nxxxi551@gmail.com>
2020-03-11 02:01:56 +0900

    chore: rubocopによる修正

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,8 +1,6 @@
 class Api::V1::ArticlesController < ApplicationController
-
   def index
     @articles = Article.all
-    @articles = @articles.map{|art| { id: art.id, title: art.title }}
-    #render json: @articles
+    @articles = @articles.map { |art| { id: art.id, title: art.title } }
   end
 end
