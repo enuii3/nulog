ec2bd86b078e39afa2ca5ec6a840ff2f9a33bc17
nonress <b.nxxxi551@gmail.com>
2020-03-11 02:03:42 +0900

    chore: rubocopで検知されないsyntaxの修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :articles
 
-  namespace :api, { format: 'json'} do
+  namespace :api, { format: 'json' } do
     namespace :v1 do
       resources :articles
     end
