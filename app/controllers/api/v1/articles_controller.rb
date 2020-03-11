c46d8eabe9ffd8904dfd22fa722d9d0e334af464
nonress <b.nxxxi551@gmail.com>
2020-03-11 13:15:30 +0900

    chore: APIコントローラー側でパラメーターの選定処理を削除

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::ArticlesController < ApplicationController
   def index
     @articles = Article.all
-    @articles = @articles.map { |art| { id: art.id, title: art.title } }
   end
 end
