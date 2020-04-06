377c2e13154f639cac1f5838c482ad44bf8dd746
nonress <b.nxxxi551@gmail.com>
2020-04-06 16:55:16 +0900

    fix: コメントと記事モデルのカラムにnull: falseを追加

--- db/migrate/20200205025158_create_articles.rb
+++ db/migrate/20200205025158_create_articles.rb
@@ -1,8 +1,8 @@
 class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
-      t.string :title, limit: 200
-      t.text :body
+      t.string :title, limit: 200, null: false
+      t.text :body, null: false
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
