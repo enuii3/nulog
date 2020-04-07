e62c88d32b08ae14e2d1b1a93c04f1547bae04dd
nonress <b.nxxxi551@gmail.com>
2020-04-07 13:27:29 +0900

    feat: 記事投稿機能と画面の構築

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -2,4 +2,19 @@ class Api::V1::ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def create
+    article = Article.new(params_article)
+    if article.save
+      render json: article
+    else
+      render json: article.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def params_article
+    params.require(:article).permit(:body, :title, :user_id)
+  end
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,3 +1,5 @@
 class ArticlesController < ApplicationController
   def index; end
+
+  def new; end
 end
