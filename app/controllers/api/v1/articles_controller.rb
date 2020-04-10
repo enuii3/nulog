4a91b3db7d891d2246664b74e281281e4b62eb37
enuii3 <46844364+enuii3@users.noreply.github.com>
2020-04-10 15:31:47 +0900

    feat: 記事詳細画面の構築 (#52)

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,5 +1,15 @@
 class Api::V1::ArticlesController < ApplicationController
+  before_action :find_article, only: :show
+
   def index
     @articles = Article.all
+  end
+
+  def show; end
+
+  private
+
+  def find_article
+    @article = Article.find(params[:id])
   end
 end
