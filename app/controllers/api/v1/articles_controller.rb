cfbed1ce1d7592974668c193ec8475292cda5f95
enuii3 <46844364+enuii3@users.noreply.github.com>
2020-04-13 17:14:40 +0900

    feat: 記事投稿画面の構築 (#54)
    * feat: 記事投稿機能と画面の構築
    
    * feat: axiosのリクエストにCSRF対策を追加
    
    * fix: 不要なコードの削除、修正
    
    * fix: 適切な修正
    
    * fix: エラー文を表示する際の修正
    
    * fix: masterをmergeした際のコンフリクトの修正

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -5,9 +5,22 @@ class Api::V1::ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def create
+    article = Article.new(params_article)
+    if article.save
+      render json: article
+    else
+      render json: article.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
   def show; end
 
   private
+
+  def params_article
+    params.require(:article).permit(:body, :title, :user_id)
+  end
 
   def find_article
     @article = Article.find(params[:id])
