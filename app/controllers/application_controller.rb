a8f8a812816acebb7733fbe7238d03e150498d5f
nonress <b.nxxxi551@gmail.com>
2020-04-14 23:44:16 +0900

    feat: Exception時のエラー表示の設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  rescue_from Exception, with: :error_response
+
+  private
+
+  def error_response
+    render json: { message: 'エラーが発生しました。システム管理者にお問い合わせください。' }, status: :internal_server_error
+  end
 end
