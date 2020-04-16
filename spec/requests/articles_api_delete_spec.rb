0a5d18858b0e0fc69df5f600d27ab2d8426cc84b
nonress <b.nxxxi551@gmail.com>
2020-04-16 17:01:21 +0900

    fix: 一つのブロックあたり25行制限の解除

--- spec/requests/articles_api_delete_spec.rb
+++ spec/requests/articles_api_delete_spec.rb
@@ -4,9 +4,19 @@ RSpec.describe 'ArticlesApiDelete', type: :request do
   let!(:article) { FactoryBot.create(:article) }
 
   describe 'ApiDelete' do
-    it 'success destroy article' do
+    it 'destroy article' do
       expect { delete "/api/v1/articles/#{article.id}" }.to change(Article, :count).by(-1)
       expect(response.status).to eq(200)
+    end
+  end
+
+  describe 'AnomaryApiDelete' do
+    it 'destroy not found article' do
+      delete "/api/v1/articles/#{Article.count + 1}"
+      json = JSON.parse(response.body)
+      
+      expect(json).to include("message" => "エラーが発生しました。システム管理者にお問い合わせください。")
+      expect(response.status).to eq(500)
     end
   end
 end
