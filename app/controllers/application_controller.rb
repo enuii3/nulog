f52746d68c6558bb89cb43e7467db108e765dd4e
nonress <b.nxxxi551@gmail.com>
2020-04-18 12:47:48 +0900

    fix: レコードが見つからない場合のエラーレスポンスの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
-  rescue_from Exception, with: :error_response
+  rescue_from Exception, with: :exception_response
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
 
   private
 
-  def error_response
+  def exception_response
     render json: { message: 'エラーが発生しました。システム管理者にお問い合わせください。' }, status: :internal_server_error
+  end
+
+  def not_found_response
+    render json: { message: 'ページが見つかりません' }, status: :not_found
   end
 end
