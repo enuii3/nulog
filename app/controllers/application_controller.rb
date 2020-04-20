86dc9727189e4d689cc2d18da94122c6618bc292
nonress <b.nxxxi551@gmail.com>
2020-04-20 23:14:48 +0900

    fix: Exception発生時に実行されるメソッド名の変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
-  rescue_from Exception, with: :error_response
+  rescue_from Exception, with: :render_status_500
 
   private
 
-  def error_response
+  def render_status_500
     render json: { message: 'エラーが発生しました。システム管理者にお問い合わせください。' }, status: :internal_server_error
   end
 end
