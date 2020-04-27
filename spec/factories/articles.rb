ff56eb9a52911c1d0cd11af7e72ed2b245e6d607
nonress <b.nxxxi551@gmail.com>
2020-04-27 16:00:07 +0900

    fix: FactoryBotで記事作成時にコメントも作成する修正

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -3,5 +3,9 @@ FactoryBot.define do
     title { Faker::Lorem.sentence }
     body { Faker::Lorem.paragraph_by_chars }
     association :user
+
+    after(:create) do |article|
+      FactoryBot.create_list(:comment, 3, article: article)
+    end
   end
 end
