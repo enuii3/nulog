a4a08a418522083c0aa1b7ab1326c86a02e23714
nonress <b.nxxxi551@gmail.com>
2020-05-07 14:43:01 +0900

    fix: コメントモデルにcommenter_nameカラムを追加

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,5 @@ class Comment < ApplicationRecord
   belongs_to :article
   has_many :favorite, as: :favoriteable, dependent: :destroy
   validates :body, presence: true, length: { maximum: 1000 }
+  validates :commenter_name, presence: true
 end
