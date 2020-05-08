8818f33ed1516b10be74da78d0f186ee84a0da8f
nonress <b.nxxxi551@gmail.com>
2020-05-08 19:44:49 +0900

    feat: コメント削除機能の構築

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   namespace :api, { format: 'json'} do
     namespace :v1 do
       resources :articles
-      resources :comments, only: [:index, :create]
+      resources :comments, only: [:index, :create, :destroy]
     end
   end
 end
