c2000db8157b14d86159097eda89387c91910435
nonress <b.nxxxi551@gmail.com>
2020-05-18 06:37:19 +0900

    feat: ヘッダーをコンポーネント化

---
+++ app/assets/javascript/header.js
@@ -0,0 +1,25 @@
+Vue.component('header-component', {
+  props: {
+    isSignedInUser: {
+      type: Boolean,
+      default: false,
+    }
+  },
+  template: `
+    <header>
+      <h1 class="logo">nulog</h1>
+      <ul class="nav-list">
+        <li class="nav-list-item">
+          <a href="/sign_out" class="articles-link" v-if="isSignedInUser">ログアウト</a>
+          <a href="/sign_in" class="articles-link" v-else>ログイン</a>
+        </li>
+        <li class="nav-list-item">
+          <a href="/articles/new" class="articles-link">記事投稿</a>
+        </li>
+        <li class="nav-list-item">
+          <a href="/" class="articles-link">記事一覧</a>
+        </li>
+      </ul>
+    </header>
+  `
+})
